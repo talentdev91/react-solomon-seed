62bd2064abbea7cb08fd2513081867a9cf618dec
goldsun27 <todayhonest27@gmail.com>
2015-01-30 12:50:36 -0800

    cleanup

--- server/security/auth.js
+++ server/security/auth.js
@@ -3,17 +3,25 @@ var userStore = require('../stores/user');
 
 module.exports = function (req, res, next) {
   if (req.cookies.auth) {
+    // decode the token into a user id
     var data = tokens.decodeToken(req.cookies.auth);
 
-    // get the user
+    // no token or can't be decoded, no auth
+    if (!data) {
+      return next();
+    }
+
+    // get the user from the data store by the user id
     userStore.getUserById(data, function(err, user) {
       if (err) {
         // todo : solomon : log it
         res.send(500, "Error");
       } else {
+        // we got a user, attach it to the request
         if (user) {
           req.user = user;
         } else {
+          // the user isn't valid, kill the cookie
           res.clearCookie('auth');
         }
 
